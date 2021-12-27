e18a24a88f9430bb383e38bf5cf70dfb17bad5bb
Kaplesh, Rahul (DNDL) <rahul.kaplesh@smithsdetection.com>
2021-12-27 22:07:21 +0530

    Adding new path finder card to do operations on cesium globe ...
    Signed-off-by: rahulkaplesh@gmail.com

--- src/app/simulator/path-finder/path-finder.component.ts
+++ src/app/simulator/path-finder/path-finder.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, Component, HostListener, OnInit } from '@angular/core';
+import { NbIconLibraries } from '@nebular/theme';
 
 @Component({
   selector: 'ngx-path-finder',
   templateUrl: './path-finder.component.html',
   styleUrls: ['./path-finder.component.scss']
 })
-export class PathFinderComponent implements OnInit {
+export class PathFinderComponent implements OnInit, AfterContentInit {
 
-  constructor() { }
+  cardTopPosition: number = 86;
+  padding: number = 10; // Value in pixels change at path-finder.component.scss file too !!
+  cardLeftPosition: number = 90;
+
+  constructor(iconsLibrary: NbIconLibraries) { 
+    iconsLibrary.registerFontPack('far', { packClass: 'far', iconClassPrefix: 'fa' });
+  }
 
   ngOnInit(): void {
+  }
+
+  ngAfterContentInit(): void {
+    this.cardTopPosition = document.getElementById('simulatorAppHeader').offsetHeight + this.padding;
+    this.cardLeftPosition = window.innerWidth - (document.getElementById('pathfinder-card-id').offsetWidth + this.padding);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.cardTopPosition = document.getElementById('simulatorAppHeader').offsetHeight + this.padding;
+    this.cardLeftPosition = window.innerWidth - (document.getElementById('pathfinder-card-id').offsetWidth + this.padding);
   }
 
 }
--- src/app/simulator/simulator.module.ts
+++ src/app/simulator/simulator.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NbMenuModule } from '@nebular/theme';
+import { NbAccordionModule, NbIconModule, NbMenuModule } from '@nebular/theme';
 import { SimulatorComponent } from './simulator.component';
 
 import { ThemeModule } from '../@theme/theme.module';
 import { SimulatorRoutingModule } from './simulator-routing.module';
 import { NbCardModule } from '@nebular/theme';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AngularCesiumModule, AngularCesiumWidgetsModule } from 'angular-cesium';
 import { PathFinderComponent } from './path-finder/path-finder.component';
@@ -21,8 +23,12 @@ import { PathFinderComponent } from './path-finder/path-finder.component';
     ThemeModule,
     SimulatorRoutingModule,
     NbCardModule,
+    MatButtonModule,
     AngularCesiumModule.forRoot({fixEntitiesShadows: false, customPipes: []}),
     AngularCesiumWidgetsModule,
+    NbIconModule,
+    NbAccordionModule,
+    MatInputModule,
   ],
   bootstrap: [SimulatorComponent],
 })
